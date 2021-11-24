6b97b1d3423aba955d03750c763f8844c361a5cc
feitosadavi <davifeitosa.dev@protonmail.com>
2021-11-24 14:22:15 -0300

    chore: change animeget to musicapi

--- musicapi/musicapi/wsgi.py
+++ musicapi/musicapi/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for animeget project.
+WSGI config for musicapi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animeget.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musicapi.settings')
 
 application = get_wsgi_application()
