c854b8ff8fb68d20f52d38789e1ba1f371927bb4
feitosadavi <davifeitosa.dev@protonmail.com>
2021-11-24 17:19:48 -0300

    feat: add Album view

--- musicapi/api/views.py
+++ musicapi/api/views.py
@@ -1,7 +1,11 @@
 from rest_framework import generics
-from .models import Music
-from .serializers import MusicSerializer
+from .models import Album, Music
+from .serializers import AlbumSerializer, MusicSerializer
 
 class MusicList(generics.ListCreateAPIView):
 	queryset = Music.objects.all()
 	serializer_class = MusicSerializer
+
+class AlbumList(generics.ListCreateAPIView):
+	queryset = Album.objects.all()
+	serializer_class = AlbumSerializer
