a925d632bae75ff7445e1a5f34c659c7a80432a8
feitosadavi <davifeitosa.dev@protonmail.com>
2021-11-24 17:39:50 -0300

    feat: add one url for each view

--- musicapi/api/urls.py
+++ musicapi/api/urls.py
@@ -2,5 +2,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-		url(r'^musics/$', views.MusicList.as_view(), name='music-list')
+	url(r'^musics/$', views.MusicList.as_view(), name='music-list'),
+	url(r'^music/(?P<pk>[0-9]+)/$', views.AlbumDetail.as_view()),
+
+	url(r'^albuns/$', views.AlbumList.as_view(), name='album-list'),
+	url(r'^album/(?P<pk>[0-9]+)/$', views.AlbumDetail.as_view()),
+
+	url(r'^bands/$', views.BandList.as_view(), name='band-list'),
+	url(r'^band/(?P<pk>[0-9]+)/$', views.BandDetail.as_view()),
+
+	url(r'^members/$', views.MemberList.as_view(), name='member-list'),
+	url(r'^member/(?P<pk>[0-9]+)/$', views.MemberDetail.as_view()),
 ]
