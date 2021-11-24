dc247011a5fe35058a9dc89fb2cb6b67b46fe300
feitosadavi <davifeitosa.dev@protonmail.com>
2021-11-24 19:52:27 -0300

    chore: add admin configs

--- musicapi/api/admin.py
+++ musicapi/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Music, Album, Band, Member
+
+admin.site.register(Music)
+admin.site.register(Album)
+admin.site.register(Band)
+admin.site.register(Member)
+
 # Register your models here.
