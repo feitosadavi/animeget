8b40b372891eb5fd20fb8301be213a2e87112831
feitosadavi <davifeitosa.dev@protonmail.com>
2021-11-24 17:35:20 -0300

    feat: add Detail view to each model

--- musicapi/api/views.py
+++ musicapi/api/views.py
@@ -6,7 +6,15 @@ class MusicList(generics.ListCreateAPIView):
 	queryset = Music.objects.all()
 	serializer_class = MusicSerializer
 
+class MusicDetail(generics.RetrieveUpdateAPIView):
+	queryset = Music.objects.all()
+	serializer_class = MusicSerializer
+
 class AlbumList(generics.ListCreateAPIView):
+	queryset = Album.objects.all()
+	serializer_class = AlbumSerializer
+
+class AlbumDetail(generics.RetrieveUpdateAPIView):
 	queryset = Album.objects.all()
 	serializer_class = AlbumSerializer
 
@@ -14,6 +22,14 @@ class BandList(generics.ListCreateAPIView):
 	queryset = Band.objects.all()
 	serializer_class = BandSerializer
 
+class BandDetail(generics.RetrieveUpdateAPIView):
+	queryset = Band.objects.all()
+	serializer_class = BandSerializer
+
 class MemberList(generics.ListCreateAPIView):
+	queryset = Member.objects.all()
+	serializer_class = MemberSerializer
+
+class MemberDetail(generics.RetrieveUpdateAPIView):
 	queryset = Member.objects.all()
 	serializer_class = MemberSerializer
