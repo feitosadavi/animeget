cf42230a387e3f8764acc12b3bd1772ad89b6425
feitosadavi <davifeitosa.dev@protonmail.com>
2021-11-24 21:20:41 -0300

    feat: add athentication to the rest of the views

--- musicapi/api/views.py
+++ musicapi/api/views.py
@@ -13,27 +13,41 @@ class MusicList(generics.ListCreateAPIView):
 class MusicDetail(generics.RetrieveUpdateAPIView):
 	queryset = Music.objects.all()
 	serializer_class = MusicSerializer
+	authentication_classes = [SessionAuthentication]
+	permission_classes = (IsAuthenticated,) = MusicSerializer
 
 class AlbumList(generics.ListCreateAPIView):
 	queryset = Album.objects.all()
 	serializer_class = AlbumSerializer
+	authentication_classes = [SessionAuthentication]
+	permission_classes = (IsAuthenticated,)
 
 class AlbumDetail(generics.RetrieveUpdateAPIView):
 	queryset = Album.objects.all()
 	serializer_class = AlbumSerializer
+	authentication_classes = [SessionAuthentication]
+	permission_classes = (IsAuthenticated,)
 
 class BandList(generics.ListCreateAPIView):
 	queryset = Band.objects.all()
 	serializer_class = BandSerializer
+	authentication_classes = [SessionAuthentication]
+	permission_classes = (IsAuthenticated,)
 
 class BandDetail(generics.RetrieveUpdateAPIView):
 	queryset = Band.objects.all()
 	serializer_class = BandSerializer
+	authentication_classes = [SessionAuthentication]
+	permission_classes = (IsAuthenticated,)
 
 class MemberList(generics.ListCreateAPIView):
 	queryset = Member.objects.all()
 	serializer_class = MemberSerializer
+	authentication_classes = [SessionAuthentication]
+	permission_classes = (IsAuthenticated,)
 
 class MemberDetail(generics.RetrieveUpdateAPIView):
 	queryset = Member.objects.all()
 	serializer_class = MemberSerializer
+	authentication_classes = [SessionAuthentication]
+	permission_classes = (IsAuthenticated,)
